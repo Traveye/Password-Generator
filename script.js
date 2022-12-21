49d7d70478e182a7a3aaa988b4ac8bf16f0f3245
Big Trees <traveye@protonmail.com>
2022-12-20 21:34:40 -0800

    updated js to meet first criteria and set pword length

--- script.js
+++ script.js
@@ -5,10 +5,25 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+ 
 
-}
+} 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); 
+
+function generatePassword() {
+  let length = window.prompt("You have selected to generate a password! Your password must be no shorter than 8 charachters and no longer than 128. How long do you want your password to be?");
+
+  if (isNaN(length) || length < 8 || length > 128 ) {
+    alert("You have entered a number outside the acceptable range or have entered something other than a number. Please try again.")
+    return;
+  }
+  else  
+
+
+  
+
+
+}
