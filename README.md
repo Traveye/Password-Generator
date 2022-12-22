1bbaea34c8626b79578e7b8714c18ae19e2ddc98
Big Trees <traveye@protonmail.com>
2022-12-22 12:35:25 -0500

    added character criteria along with array and set up answers array

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Password-Generator
+
+## Goals
+The goals of this project was to create a password generator using JavaScript that would generate passwords of a certain length and character type based on user input. In order for the project to be successful the generator must:
+
+1. Create passwords that are betweeen 8 and 128 characthers in length. 
+2. Users can select characters to include. Options are numbers, lower case letters, upper case letters, and specail characters. 
+3. Once the password is generated it will be displayed in the given html element. 
+
+## The Build
+
+I began by creating a space for user input using `window.prompt` that would request the desired length of password from the user. This input will then be checked to ensure that it is numerical only and that it is not less than 8 nor more than 128 as directed. I also added a modulo comparision to ensure the number entered was whole. 
+
+Next I created a series of prompts using the `window.confirm` method to collect the remaining password criteria. I then ran a check to ensure at least one response had a value of true otherwise no passwor would generate. Using the `push` method, I added these boolean type responses to an array. 
+
+## Installation
+NA
+
+## Usage
+NA
+
+## Credits
+The HTML,CSS, and first 14 lines of the JS file were provided as part of course. 
--- script.js
+++ script.js
@@ -16,14 +16,58 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
   let length = window.prompt("You have selected to generate a password! Your password must be no shorter than 8 charachters and no longer than 128. How long do you want your password to be?");
 
-  if (isNaN(length) || length < 8 || length > 128 ) {
+  if (isNaN(length) || length < 8 || length > 128 || length % 1 != 0 ) {
     alert("You have entered a number outside the acceptable range or have entered something other than a number. Please try again.")
     return;
   }
-  else  
+  
+  let getNumber = window.confirm("Would you like your password to include numbers? Click ok to include or cancel to skip.");
 
+  let getLowerC = window.confirm("Would you like your password to include lowercase letters? Click ok to include or cancel to skip");
+
+  let getUpperC = window.confirm("Would you like your password to include uppercase letters? Click ok to include or cancel to skip");
+
+  let getSpecialC = window.confirm("Would you like your password to include special characters? Click ok to include or cancel to skip");
+
+  var answers = [];
+
+  answers.push(getNumber, getLowerC, getUpperC, getSpecialC);
+
+  // console.log(answers);
+
+  // if (answers[0] && answers[1] && answers[2] && answers[3] == false) {
+  //   alert("Oh no! You must select at least one type for a password to generate. Please try again.");
+  //   return;
+  // }
+  
+  var selection = [];
+  var numbers = [0, 1, 2, 3, 4 , 5, 6, 7, 8, 9,];
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+  var upperCase = ["A", "B", "C", "D", "E", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  var specialChar = ["!", "@", "#", "$", "%"]
+
+  if (getNumber == true) {
+    selection.push(numbers);
+  }
+
+  if (getLowerC == true) {
+    selection.push(lowerCase);
+  }
+
+  if (getUpperC == true) {
+    selection.push(upperCase);
+  }  
+
+  if (getSpecialC == true) {
+    selection.push(specialChar);
+   
+  }
+  console.log(selection);
+  console.log(length)
 
   
 
-
 }
+  
+
+
